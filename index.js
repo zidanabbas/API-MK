db3651006094eb712b749877e723dd8fafb2f163
zidanabbas <zidan.abbas28@gmail.com>
2024-05-11 00:24:19 +0700

    fix : added user-service router and authentication middleware

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ import "dotenv/config";
 import prisma from "./prisma/index.js";
 import cors from "cors";
 import middlewareLogRequest from "./middleware/log.js";
+import { authMiddleware } from "./middleware/auth-middleware.js";
 import projectRoutes from "./routes/products.js";
 import usersRoutes from "./routes/users.js";
+import userServices from "./routes/auth.js";
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(middlewareLogRequest);
+
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://nasigorengmk.vercel.app/"],
@@ -24,18 +27,18 @@ app.get("/v1", (req, res) => {
 });
 
 app.use("/v1", projectRoutes);
-app.use("/v1", usersRoutes);
+app.use("/v1", userServices);
+app.use("/v1", authMiddleware, usersRoutes);
 
 prisma
   .$connect()
   .then(() => {
     console.log("Connected to MongoDB");
-    // Memulai server setelah koneksi berhasil di buka
     app.listen(PORT, () => {
       console.log(`express API running on PORT ${PORT}`);
     });
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
-    process.exit(1); // Keluar dari aplikasi jika koneksi gagal
+    process.exit(1);
   });
